3b7bcec0f7a87780a7be715d209da173fdcfc6bf
Akhil Kumar <akhilskumar1999@gmail.com>
2020-05-20 11:38:54 +0530

    Login Form

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,20 @@ namespace Bookstore_Management_System
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String user = textBox1.Text;
+            String pass = textBox2.Text;
+
+            if(user.Equals("admin") && pass.Equals("123"))
+            {
+                MessageBox.Show("Login Succcess");
+            }
+            else
+            {
+                MessageBox.Show("Login Unsuccessful");
+            }
+        }
     }
 }
