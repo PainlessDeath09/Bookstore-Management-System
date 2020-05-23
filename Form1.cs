f243066b9f8935490e886a355f7b0996aa2c0e5d
Akhil Kumar <akhilskumar1999@gmail.com>
2020-05-23 09:07:12 +0530

    Database configured

--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Bookstore_Management_System
 {
@@ -17,8 +18,24 @@ namespace Bookstore_Management_System
             InitializeComponent();
         }
 
+        SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"|DataDirectory|\\Database.mdf\";Integrated Security=True");
+        SqlCommand cmd;
+        SqlDataReader dr;
+
+        private void getData()
+        {
+            con.Open();
+            String syntax = "SELECT Value from systemTable where Property = 'UserName'";
+            cmd = new SqlCommand(syntax, con);
+            dr = cmd.ExecuteReader();
+            dr.Read();
+            MessageBox.Show(""+dr[0].ToString());
+
+        }
+        
         private void button1_Click(object sender, EventArgs e)
         {
+            getData();
             String user = textBox1.Text;
             String pass = textBox2.Text;
 
